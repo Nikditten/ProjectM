24bed67d619454eb64c8d5a0af316e580e5b168c
Nikoditten <48121115+Nikditten@users.noreply.github.com>
2023-03-19 12:05:34 +0100

    work

---
+++ ProjectM/Repositories/TaskDBRepository.swift
@@ -0,0 +1,13 @@
+//
+//  TaskRepository.swift
+//  ProjectM
+//
+//  Created by Niklas Børner on 19/03/2023.
+//
+
+
+protocol TaskDBRepository {
+    
+    func getTasks() -> AnyPublisher<
+    
+}
